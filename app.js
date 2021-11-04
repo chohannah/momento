c312e75da9a5abdf944fbc7793d18cca5909df69
Jinnie Cho <yejincho1105@gmail.com>
2021-11-04 21:54:52 +0900

    💫 Enabled to save userName in localStorage depending on saving history

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+// common contstants
+
 const loginForm = document.querySelector('.login-form')
 const loginInput = document.querySelector('.login-form .login-input')
 const greeting = document.querySelector('.greeting')
 
+// common string-only constants
+
 const HIDDEN_CLASSNAME = 'visually-hidden'
+const USERNAME_KEY = 'userName'
+
+// common functions
+
+function paintGreetings(userName) {
+  greeting.innerText = `Hello ${userName}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+// When username DOESN'T exists
 
 function onLoginSubmit(e) {
   e.preventDefault()
   loginForm.classList.add(HIDDEN_CLASSNAME)
   const userName = loginInput.value
-  greeting.innerText = `Hello ${userName}`
-  greeting.classList.remove(HIDDEN_CLASSNAME)
+  localStorage.setItem(USERNAME_KEY, userName)
+  paintGreetings(userName)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit)
+// When username exists
+
+const savedUserName = localStorage.getItem(USERNAME_KEY)
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  paintGreetings(savedUserName)
+}
