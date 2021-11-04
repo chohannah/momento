39980e7e3d208244459453ffd4ebe94a08ebd3fd
Jinnie Cho <yejincho1105@gmail.com>
2021-11-04 17:43:30 +0900

    🚧 Handled className to show greeting message with userName

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector('.login-form')
 const loginInput = document.querySelector('.login-form .login-input')
+const greeting = document.querySelector('.greeting')
+
+const HIDDEN_CLASSNAME = 'visually-hidden'
 
 function onLoginSubmit(e) {
   e.preventDefault()
-  console.log(e)
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const userName = loginInput.value
+  greeting.innerText = `Hello ${userName}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
