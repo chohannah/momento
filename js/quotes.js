98060cf423794250372d86d025304ae6d6cfd7c5
Jinnie Cho <yejincho1105@gmail.com>
2021-11-26 00:01:42 +0900

    ♻️ Organised indexes and added new index

--- js/quotes.js
+++ js/quotes.js
@@ -1,50 +1,62 @@
 const quotes = [
   {
-    index: 1,
+    index: 0,
     quote:
       'Ideas don’t come out fully formed. They only become clear as you work on them. You just have to get started.',
     author: 'Mark Zukerberg',
   },
   {
-    index: 2,
+    index: 1,
     quote: 'The greatest successes come from having the freedom to fail.',
     author: 'Mark Zukerberg',
   },
   {
-    index: 3,
+    index: 2,
     quote: 'A person who has a why to live can bear almost any how.',
     author: 'Friedrich Nietzsche',
   },
   {
-    index: 4,
+    index: 3,
     quote: "The scholar's greatest weakness: calling procrastination research.",
     author: 'Stephen King',
   },
   {
-    index: 5,
+    index: 4,
     quote: 'Excellence is the gradual result of always striving to do better.',
     author: 'Pat Riley',
   },
   {
-    index: 6,
+    index: 5,
     quote: "Coincidence is God's way of remaining anonymous",
     author: 'Albert Einstein',
   },
   {
-    index: 7,
+    index: 6,
     quote: 'Where the willingness is great the difficulties cannot be great.',
     author: 'Niccolo Machiavelli',
   },
   {
-    index: 8,
+    index: 7,
     quote:
       'Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.',
     author: 'Dale Carnegie',
   },
   {
-    index: 9,
+    index: 8,
     quote:
       'If you find yourself constantly trying to prove your worth to someone, you have already forgotten your value.',
     author: 'Unkown',
+  },
+  {
+    index: 9,
+    quote:
+      'No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.',
+    author: 'Buddha',
+  },
+  {
+    index: 10,
+    quote:
+      'There are only two mistakes one can make along the road to truth; not going all the way, and not starting.',
+    author: 'Buddha',
   },
 ]
