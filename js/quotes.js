2e9a1dc2b97ce12f6fc140f8ffe2be01343cda10
Jinnie Cho <yejincho1105@gmail.com>
2021-11-25 23:11:49 +0900

    ✨ Created quotes database

---
+++ js/quotes.js
@@ -0,0 +1,50 @@
+const quotes = [
+  {
+    index: 1,
+    quote:
+      'Ideas don’t come out fully formed. They only become clear as you work on them. You just have to get started.',
+    author: 'Mark Zukerberg',
+  },
+  {
+    index: 2,
+    quote: 'The greatest successes come from having the freedom to fail.',
+    author: 'Mark Zukerberg',
+  },
+  {
+    index: 3,
+    quote: 'A person who has a why to live can bear almost any how.',
+    author: 'Friedrich Nietzsche',
+  },
+  {
+    index: 4,
+    quote: "The scholar's greatest weakness: calling procrastination research.",
+    author: 'Stephen King',
+  },
+  {
+    index: 5,
+    quote: 'Excellence is the gradual result of always striving to do better.',
+    author: 'Pat Riley',
+  },
+  {
+    index: 6,
+    quote: "Coincidence is God's way of remaining anonymous",
+    author: 'Albert Einstein',
+  },
+  {
+    index: 7,
+    quote: 'Where the willingness is great the difficulties cannot be great.',
+    author: 'Niccolo Machiavelli',
+  },
+  {
+    index: 8,
+    quote:
+      'Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.',
+    author: 'Dale Carnegie',
+  },
+  {
+    index: 9,
+    quote:
+      'If you find yourself constantly trying to prove your worth to someone, you have already forgotten your value.',
+    author: 'Unkown',
+  },
+]
